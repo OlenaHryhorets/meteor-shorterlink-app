b491195b9030b444b4dca6308edf12e318df32dd
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-12 19:24:30 +0200

    added checking url, insert data if valid

--- client/components/LinkForm.jsx
+++ client/components/LinkForm.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class LinkForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { error: ''};
+    }
+
     handleSubmit = (event) => {
         event.preventDefault();
         console.log(event.target.input.value);
-        Meteor.call('links.insert', event.target.input.value);
+        Meteor.call('links.insert', event.target.input.value, (error) => {
+            if(error) {
+                this.setState({error: 'Enter a valid URL'});
+            } else {
+                this.setState({error: ''});
+                event.target.reset();
+            }
+        });
     }
 
     render() {
@@ -14,6 +27,7 @@ class LinkForm extends Component {
                    <label>Link to shorten</label>
                    <input id="input" className="form-control"></input>
                </div>
+               <div className="text-danger">{this.state.error}</div>
                <button className="btn btn-primary">Shorten!</button>
             </form>
         );
--- imports/api/links.js
+++ imports/api/links.js
@@ -4,11 +4,16 @@ import { check, Match } from 'meteor/check';
 
 Meteor.methods({
     'links.insert': function(url) {
-        console.log('attempting to save ' + url);
+      console.log("attempting to save " + url);
 
-        // Validation inside method
-        // validUrl.isUri(url)
-        check(url, Match.Where(url => validUrl.isUri(url)));
+      // Validation inside method
+      // validUrl.isUri(url)
+      check(url, Match.Where((url) => validUrl.isUri(url))); //will throw import { Links } from '../imports/api/links' to both sides
+
+      // ready to save the url
+
+      const token = Math.random().toString(36).slice(-5);
+      Links.insert({ url: url, token: token, clicks: 0 });
     }
 })
 
