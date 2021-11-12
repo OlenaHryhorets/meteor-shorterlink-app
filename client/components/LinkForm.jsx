9d7e638b5dc4e10b21a5b05b5c8dde579762a262
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-12 18:14:05 +0200

    LinkForm class component

---
+++ client/components/LinkForm.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class LinkForm extends Component {
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event.target.input.value)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+               <div className="form-group">
+                   <label>Link to shorten</label>
+                   <input id="input" className="form-control"></input>
+               </div>
+               <button className="btn btn-primary">Shorten!</button>
+            </form>
+        );
+    }
+}
+
+export default LinkForm;
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from '../../client/components/Header';
+import LinkForm from '../../client/components/LinkForm';
 
 export const App = () => (
   <div>
     <Header />
+    <LinkForm />
   </div>
 );
