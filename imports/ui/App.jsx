899d12522f74be32cb088c9607718eaab7e5bc03
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-12 16:11:12 +0200

    added bootstrap

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Header from '../../client/components/Header';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <Header />
   </div>
 );
