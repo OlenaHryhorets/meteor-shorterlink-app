86dc5e7a8836a4539dbaed615a74006b0eaf9b2c
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-12 16:05:56 +0200

    heaader component

---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default Header = () => {
+    return (
+      <nav className="nav navbar-default">
+        <div className="nav navbar-header">
+          <a className="navbar-brand">ShortenMyLink</a>
+        </div>
+      </nav>
+    );
+}
