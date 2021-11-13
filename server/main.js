0ed4a409e959651a1d6162fe928b27798cedc5a5
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-13 14:15:52 +0200

    handling route with token, redirect to long link on server side before load react app, increment clicks on link visits

--- server/main.js
+++ server/main.js
@@ -13,8 +13,27 @@ Meteor.startup(() => {
   })
 });
 
-const middleware = ConnectRoute(function(router) {
-  router.get('/:token', (req) => console.log(req));
-})
+// executed whenever user visits a route like "localhost:3000/abcd"
+function onRoute(req, res, next) {
+ 
+  // take the token out of the url and try to a find a matching in the Links collection
+  const link = Links.findOne({ token: req.params.token });
+  if (link) {
+    // update record in DB with mongo modifier
+    Links.update(link, { $inc: { clicks: 1 }});
+    // if we find a link object then redirect the user to the ling URL
+    res.writeHead(307, { 'Location': link.url })
+    res.end();
+
+  } else {
+    // if we don't find a link object send the user to our normal React app
+    next();
+  }
+}
+
+// if route match use the onRoute handler
+const middleware = ConnectRoute( function(router) {
+  router.get('/:token', onRoute);
+});
 
 WebApp.connectHandlers.use(middleware);
